4a836e33c17ba61d97f9de08390e3179d51e7e78
Arnold <arnold9525@gmail.com>
2019-05-12 17:16:48 +0200

    Preparo entorno para trabajr con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgB1voqzjk43ILne8IHhnnYiLEaICmMOI',
+    authDomain: 'platzinger-f249d.firebaseapp.com',
+    databaseURL: 'https://platzinger-f249d.firebaseio.com',
+    projectId: 'platzinger-f249d',
+    storageBucket: 'platzinger-f249d.appspot.com',
+    messagingSenderId: '288730387316',
+    // appId: '1:288730387316:web:c8c8da12ce746a8d'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCgB1voqzjk43ILne8IHhnnYiLEaICmMOI',
+    authDomain: 'platzinger-f249d.firebaseapp.com',
+    databaseURL: 'https://platzinger-f249d.firebaseio.com',
+    projectId: 'platzinger-f249d',
+    storageBucket: 'platzinger-f249d.appspot.com',
+    messagingSenderId: '288730387316',
+    // appId: '1:288730387316:web:c8c8da12ce746a8d'
+  }
 };
 
 /*
