816f212736dd6ad130f174d3b1af1a8e0f8e814f
Arnold <arnold9525@gmail.com>
2019-07-16 15:17:36 +0200

    Creo módulo authentication y sus componentes.

---
+++ src/app/components/authentication/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+
+  public operationType: string = 'login';
+  public email: string = null;
+  public password: string = null;
+  public nick: string = null;
+
+  constructor(private authenticationService: AuthenticationService,
+    private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password)
+      .then((data) => {
+        alert('Logeado correctamente');
+        console.log(data);
+        this.router.navigate(['home']);
+      }).catch((error) => {
+        alert('error');
+        console.log(error);
+      });
+  }
+
+
+}
