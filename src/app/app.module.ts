8362cf6c0dae5e3a0d9d09bbd6dddb2e2ae6a232
Arnold <arnold9525@gmail.com>
2019-07-16 15:34:30 +0200

    Elimino ruta residual.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ConversationComponent } from './components/conversation/conversation.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -25,7 +24,6 @@ import { ContactComponent } from './components/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
     ConversationComponent,
     ProfileComponent,
